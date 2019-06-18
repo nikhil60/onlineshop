29cb765948bc5325e30e47da3944966fc61bcfb7
Nikhil Chavan <nikhil.chavan@nihilent.com>
2019-06-18 12:20:43 +0530

    Exception handling for @Valid

--- src/main/java/com/retail/onlineshop/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/retail/onlineshop/exception/CustomResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.retail.onlineshop.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +25,17 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 		exceptionResponse.setStatus(HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<Object>(exceptionResponse,HttpStatus.NOT_FOUND);
 	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+	            MethodArgumentNotValidException exception,
+	            HttpHeaders headers,
+	            HttpStatus status,
+	            WebRequest request) {
+	        ExceptionResponse exceptionResponse = new ExceptionResponse();
+			exceptionResponse.setTimestamp(LocalDateTime.now());
+			exceptionResponse.setError(exception.getMessage());
+			exceptionResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+	        return new ResponseEntity(exceptionResponse, headers, HttpStatus.BAD_REQUEST);
+	    }
 }
